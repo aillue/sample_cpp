0551256d71845281f08ba795cd4689db4deafaac
aillue <jcc03713@nifty.com>
2014-03-26 14:11:14 +0900

    add move test for unique_ptr

--- sample_cpp/test_memory.cpp
+++ sample_cpp/test_memory.cpp
@@ -3,16 +3,27 @@
 
 #include <memory>
 
-TEST ( TestMemory, shared_ptr )
+TEST ( shared_ptr, default_value )
 {
 	std::shared_ptr<int> a;
 
 	ASSERT_EQ ( nullptr, a );
 }
 
-TEST ( TestMemory, unique_ptr )
+TEST ( unique_ptr, unique_ptr )
 {
 	std::unique_ptr<int> a( new int( 10 ) );
 
 	ASSERT_EQ ( 10, *a );
+}
+
+TEST ( unique_ptr, move )
+{
+	std::unique_ptr<char> a( new char( 'a' ) );
+	std::unique_ptr<char> b;
+
+	b = std::move( a );
+
+	ASSERT_EQ ( nullptr, a );
+	ASSERT_EQ ( 'a', *b );
 }
